dda2e611019b7714c70f7367f4c62b12e7836d35
eorichards <eorichar@ucsd.edu>
2018-02-08 11:06:58 -0800

    Website

--- routes/add.js
+++ routes/add.js
@@ -2,4 +2,14 @@ var data = require("../data.json");
 
 exports.addFriend = function(req, res) {    
 	// Your code goes here
+	var name = req.query.name;
+	var descrip = req.query.description;
+	data.friends.push({
+		'name': name,
+		'description': descrip,
+		'imageURL': "http://lorempixel.com/400/400/people"
+	});
+	console.log(data);
+		res.render('index', data);
+
  }
